aa0411b271a9e29e8cd6947533598e9bd25ebca9
Arvind6353 <arvindan92@gmail.com>
2016-12-03 23:23:56 +0530

    c

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ io.on('connection', function(socket){
 
 	socket.on('join:room', function(data){
 		var room_name = data.room_name;
+		msg.text = msg.user + ' has joined the room';
 		socket.join(room_name);
+
+		socket.in(msg.room).emit('message', msg);
 	});
 
 
 	socket.on('leave:room', function(msg){
 		msg.text = msg.user + ' has left the room';
+
 		socket.leave(msg.room);
 		socket.in(msg.room).emit('message', msg);
 	});
