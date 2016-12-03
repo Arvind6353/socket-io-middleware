6f6d465539ff3505eed80f3e3c9d9e7ec3b646c1
Arvind6353 <arvindan92@gmail.com>
2016-12-03 14:52:01 +0530

    c

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-var io = require('socket.io')(process.env.PORT);
+ var http = require('http');
+var fs = require('fs');
+var socketio = require('socket.io');
+var html = require('escape-html');
+
+var port = process.env.PORT || 3000;
+
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+server.listen(port);
+app.use( require('express').static(__dirname ));
 
 io.on('connection', function(socket){
 
